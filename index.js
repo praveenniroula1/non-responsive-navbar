b1035c808397ea0e3ccb005e598664d5ad7fe449
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2022-12-17 10:37:50 +1100

    calculaor layout

---
+++ index.js
@@ -0,0 +1,9 @@
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    console.log(buttonText);
+  });
+}
